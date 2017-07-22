76ec0e12788a2fae9d3bb2cd1eba7abe73b2fd39
Stephen Holdaway <stecman@gmail.com>
2017-07-22 17:04:36 +1200

    Replace custom json_encode implementation with PHP 5.4 json options
    Options to pretty-print and output JSON without escaped slashes have been
    available since PHP 5.4. Symfony Console 3.x has a minimum PHP version
    requirement of 5.5.9, so these options are now available to us.

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -66,81 +66,12 @@ class InitCommand extends SymfonyCommand
         );
 
         // Save template
-        file_put_contents('beam.json', str_replace('\/', '/', $this->jSONFormat(json_encode($template))));
+        file_put_contents('beam.json', json_encode($template, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
         $output->writeln(
             "<info>Success: beam.json file saved to " . getcwd() .
             "/beam.json - be sure to check the file before using it</info>"
         );
 
-    }
-
-    /**
-     * @param $json
-     * @return bool|string
-     */
-    private function jSONFormat($json)
-    {
-        $tab = "\t";
-        $new_json = "";
-        $indent_level = 0;
-        $in_string = false;
-
-        $json_obj = json_decode($json);
-
-        if ($json_obj === false) {
-            return false;
-        }
-
-        $json = json_encode($json_obj);
-        $len = strlen($json);
-
-        for ($c = 0; $c < $len; $c++) {
-            $char = $json[$c];
-            switch ($char) {
-                case '{':
-                case '[':
-                    if (!$in_string) {
-                        $new_json .= $char . "\n" . str_repeat($tab, $indent_level + 1);
-                        $indent_level++;
-                    } else {
-                        $new_json .= $char;
-                    }
-                    break;
-                case '}':
-                case ']':
-                    if (!$in_string) {
-                        $indent_level--;
-                        $new_json .= "\n" . str_repeat($tab, $indent_level) . $char;
-                    } else {
-                        $new_json .= $char;
-                    }
-                    break;
-                case ',':
-                    if (!$in_string) {
-                        $new_json .= ",\n" . str_repeat($tab, $indent_level);
-                    } else {
-                        $new_json .= $char;
-                    }
-                    break;
-                case ':':
-                    if (!$in_string) {
-                        $new_json .= ": ";
-                    } else {
-                        $new_json .= $char;
-                    }
-                    break;
-                case '"':
-                    if ($c > 0 && $json[$c - 1] != '\\') {
-                        $in_string = !$in_string;
-                    }
-                // Fall through
-                default:
-                    $new_json .= $char;
-                    break;
-            }
-        }
-
-        return $new_json;
     }
 }
